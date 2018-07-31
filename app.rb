7b933f3d5052863ae3a063fbe2300d9820709d51
barnesemily12@gmail.com <barnesemily12@gmail.com>
2018-07-31 18:55:37 +0000

    automatically backed up by learn

--- app.rb
+++ app.rb
@@ -11,10 +11,9 @@ class App < Sinatra::Base
   # This is a sample dynamic route.
   get "/hello/:name" do
     @user_name = params[:name]
-    "Hello #{@persons_name}."
-    
-  
+    "Hello #{@user_name}."
 end
+
 get '/multiply/:num1/:num2'do
   num1= params[:num1].to_i
   num2= params [:num2].to_i
